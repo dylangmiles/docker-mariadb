16502d8c0d8cac04f7e2ee680182f2df5c349d11
dylangmiles <dylan.g.miles@gmail.com>
2015-06-12 16:20:13 +0200

    Allow root connection from dev vbox

--- config/mariadb-setup.sql
+++ config/mariadb-setup.sql
@@ -2,6 +2,8 @@ DELETE FROM mysql.user WHERE User='';
 DELETE FROM mysql.db WHERE Db LIKE 'test%';
 DROP DATABASE test;
 UPDATE mysql.user SET password = password('abc123') WHERE user = 'root';
-CREATE USER docker;
-GRANT ALL PRIVILEGES ON *.* To 'docker' IDENTIFIED BY 'docker';
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'172.17.42.%'
+    IDENTIFIED BY 'abc123'  
+    WITH GRANT OPTION;
+FLUSH PRIVILEGES;
 flush privileges;
